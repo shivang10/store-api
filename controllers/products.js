b06641e4ad1d8b8449f1cd42f1e9d6818d46ba9e
Shivang Garg <shivang10garg@gmail.com>
2022-05-30 08:29:37 +0530

    deletes unnecessary routes

--- controllers/products.js
+++ controllers/products.js
@@ -1,13 +1,6 @@
 const Product = require("../models/product");
 const supportedOperators = require("../utils/operators");
 
-const getAllProductsStatic = async (req, res) => {
-    const products = await Product.find({
-        title: "vase table",
-    });
-    res.status(200).json({msg: products, nbHits: products.length});
-}
-
 const getAllProducts = async (req, res) => {
     const {
         brand, title, sort, price, rating, page = 1,
@@ -114,6 +107,4 @@ const getAllProducts = async (req, res) => {
     res.status(200).json({products: finalProducts, nbHits: finalProducts.length});
 }
 
-module.exports = {
-    getAllProductsStatic, getAllProducts
-}
+module.exports = {getAllProducts}
